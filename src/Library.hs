298cf0faf2bea2c9dbac72f643fa10ab0abff417
luchoetapia <luciano.etapia@gmail.com>
2025-04-07 14:25:40 -0300

    Puntos 1 y 2

--- src/Library.hs
+++ src/Library.hs
@@ -4,27 +4,29 @@ import PdePreludat
 -- 1. Numeros
 
 siguiente :: Number -> Number
-siguiente = implementame
+siguiente numero = numero + 1
 
 esPositivo :: Number -> Bool
-esPositivo = implementame
+esPositivo n = n > 0
 
 -- escriban el tipo de esta función
-inversa n = implementame
+inversa :: Number -> Number
+inversa n = 1 / n
 
 -- 2. Temperaturas
 
 celsiusAFahrenheit :: Number -> Number
-celsiusAFahrenheit celsius = implementame
+celsiusAFahrenheit celsius = celsius * 1.8 + 32
 
 fahrenheitACelsius :: Number -> Number
-fahrenheitACelsius fahrenheit = implementame
+fahrenheitACelsius fahrenheit = (fahrenheit - 32) / 1.8
+
+haceFrioCelsius :: Number -> Bool
+haceFrioCelsius grados = grados <= 8
 
 -- escriban el tipo de esta función
-haceFrioCelsius grados = implementame
-
--- escriban el tipo de esta función
-haceFrioFahrenheit grados = implementame
+haceFrioFahrenheit :: Number -> Bool
+haceFrioFahrenheit grados = grados <= 46.4
 
 -- 2.5 Bonus OPCIONAL
 perimetroCirculo :: Number -> Number
